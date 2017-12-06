5c5e1b6227a9061a61fcae77e2fcc19d5fd4c649
glazkov <glazkov@GLAZKOV-SV.iba>
2017-12-06 12:16:46 +0300

    1-st push

--- src/testJavaProject/hello.java
+++ src/testJavaProject/hello.java
@@ -3,6 +3,9 @@ package testJavaProject;
 public class hello {
 	/**
 	 * @param args
+	 * 
+	 * 2-nd update
+	 * 
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
